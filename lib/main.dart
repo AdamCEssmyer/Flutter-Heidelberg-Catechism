a2ba1c23f71e4db227905486d9fe5b3c4737670f
Adam Essmyer <acemyer@gmail.com>
2022-04-22 21:51:29 -0500

    first push

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Heidelberg!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Heidelberg!'),
+        ),
+        body: const topLeft(
+          alignment: Alignment.topLeft,
+          child: Text('Heidelberg Catechism'),
+        ),
+      ),
+    );
+  }
+}
